a17459cb6dcbab5bace76d2120b534d154c5d557
sky <marinofecal@gmail.com>
2026-04-13 19:12:56 +0200

    Update route.js

--- app/api/bot/route.js
+++ app/api/bot/route.js
@@ -11,7 +11,7 @@ export async function POST(req) {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        model: "mixtral-8x7b-32768",
+        model: "llama-3.1-8b-instant",
         messages: [
           {
             role: "system",
