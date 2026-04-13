663e71a0479831802ac8322bcdb1d00c0a1a4371
sky <marinofecal@gmail.com>
2026-04-13 17:35:54 +0200

    Create page.jsx

---
+++ app/bots/page.jsx
@@ -0,0 +1,12 @@
+export default function Bots() {
+  return (
+    <main style={{ padding: "40px" }}>
+      <h1>Bots</h1>
+
+      <ul>
+        <li><a href="/bot?type=ifrs">IFRS Bot</a></li>
+        <li><a href="/bot?type=audit">Audit Bot</a></li>
+      </ul>
+    </main>
+  );
+}
